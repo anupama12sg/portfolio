c08cb0a70118d87d5f666dec997026eea73e9e8b
Anupama Singh <anupama12sg@gmail.com>
2024-09-16 10:41:22 +0530

    feat: Added spotlight and installed several packages.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
+import hero from "../../components/hero"
+import { div } from "framer-motion/client";
+import Hero from "../../components/hero";
 
 export default function Home() {
   return (
-
-    <main className="relative bg-black-100 flex-justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 justify-center items-center flex flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <Hero/>
+        <Hero />
       </div>
-      </main>
-    
+    </main>
   );
 }
