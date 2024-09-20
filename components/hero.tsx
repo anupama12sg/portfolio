f7bc8f4a844ec520922f5ee0aff451b66a0dbad1
Anupama Singh <anupama12sg@gmail.com>
2024-09-20 20:57:00 +0530

    chore:

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,14 +19,17 @@ const Hero = () => {
             </div>
             <div className="flex justify-center relative my-20 z-10">
                 <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-                    <h2 className= "uppercase tracking-widest text-xs text-center text-blue-600 max-w-80">
+                    <h2 className="uppercase tracking-widest text-xs text-center text-blue-600 max-w-80">
                         Dynamic Web Magic with Next.js
                     </h2>
 
                     <TextGenerateEffect
-                    className="text-center text-[40px] md: text-5xl lg:text-6xl "
-                    words="Transforming Concepts into Seamless Experiences"
+                        className="text-center text-[40px] md: text-5xl lg:text-6xl "
+                        words="Transforming Concepts into Seamless Experiences"
                     />
+                    <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                        Hi, I am Anupama Singh, a Next.js Developer based in India. 
+                    </p>
                 </div>
             </div>
         </div>
