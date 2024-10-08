c5a7920f0fe89f81d3e9011c947888638dab4c43
Anupama Singh <anupama12sg@gmail.com>
2024-10-08 21:55:26 +0530

    chore:

--- components/ui/bentoGrid.tsx
+++ components/ui/bentoGrid.tsx
@@ -152,7 +152,7 @@ export const BentoGridItem = ({
               </div>
 
               <MagicButton
-                title={copied ? 'Email copied' : 'Copy my email'}
+                title={copied ? 'Email copied!' : 'Copy my email.'}
                 icon={<IoCopyOutline />}
                 position="left"
                 otherClasses="!bg-[#161a31]"
