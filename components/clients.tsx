464a94606d5484f3ac0844bd06fcf4bc564b0bc1
Anupama Singh <anupama12sg@gmail.com>
2024-11-01 17:11:53 +0530

    chore:

--- components/clients.tsx
+++ components/clients.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Clients = () => {
     return (
-        <div>Clients</div>
+        <div className="py-20" id="projects">
+            <h1 className="heading">
+                Kind words from {' '}
+                <span className="text-purple">kind people</span>
+            </h1>
+            <div className="flex flex-col items-center justify-center">
+
+            </div>
+        </div>
     )
 }
 
