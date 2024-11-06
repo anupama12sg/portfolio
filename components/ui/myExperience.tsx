6aed18e8b67a339fdaef01db788760146ee50f43
Anupama Singh <anupama12sg@gmail.com>
2024-11-06 19:15:41 +0530

    chore: Heading for work ex

--- components/ui/myExperience.tsx
+++ components/ui/myExperience.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Experience = () => {
-return (
-<div>My Experience</div>
-)
+    return (
+        <div className="py-20" id="testimonials">
+            <h1 className="heading">
+                My {' '}
+                <span className="text-purple">Work Experience</span>
+            </h1>
+            <div className="flex flex-col items-center justify-center max-lg:mt-10"></div>
+        </div>
+    )
 }
 
-export default Experience
+export default Experience;
